873b1dad39dbf7c79a7383f2310df3d32cbe8a9b
Luís Fernandes <luisapidcloud@hotmail.com>
2017-03-23 14:27:35 +0000

    update: Reel working example

--- js/leaprezi.js
+++ js/leaprezi.js
@@ -6,31 +6,8 @@ var controller = null;
 var options = null;
 var isConnected = null;
 
-var player = new PreziPlayer('prezi-player', {
-	preziId: 'hp08thgs3ifs',
-	width: 620,
-	height: 444,
-	explorable: false,
-	controls: true
-});
 
-// when prezi step changes, set the active link accordingly
-player.on(PreziPlayer.EVENT_CURRENT_STEP, function(e) {
-	$('#sidebar-nav li').removeClass('active');
-	$("#sidebar-nav li a[href='#"+e.value+"']").parent().addClass('active');
-});
-
-// flies to the corresponding prezi step when one of the links is clicked
-$('#sidebar-nav li a').on('click', function(e){
-	e.preventDefault();
-	player.flyToStep(this.href.match('#(.*)$')[1]);
-	$('#sidebar-nav li').removeClass('active');
-	$(this).parent().addClass('active');
-});
-
-$(document).ready(function() {
-	init();
-});
+init();
 
 function init()
 {
@@ -96,24 +73,15 @@ function onFrame(frame)
 
   		hand = frame.hands[0];
 
-    	if (canDoGesture() && ExtendedFingersCount(hand) > 2) {
+  		var extendedFingers = ExtendedFingersCount(hand);
 
-    		// retrieve velocity X
-    		var velocityX = hand.palmVelocity[0];
+    	if (extendedFingers === 2) {
 
-    		// palmNormal[0] = roll
-			if (velocityX > 1000 && hand.palmNormal[0] > 0.2)
-            {
-            	console.log("left to right");
-                lastSwipe = new Date();
-                reel.stepRight;
-            }
-            else if (velocityX < -1000 && hand.palmNormal[0] < -0.2)
-            {
-            	console.log("right to left");
-                lastSwipe = new Date();
-                reel.stepLeft;
-            }
+    		$('#image').trigger("stepLeft");
+
+    	}
+    	else if (extendedFingers > 2) {
+    		$('#image').trigger("stepRight");
     	}
   	}
 }
